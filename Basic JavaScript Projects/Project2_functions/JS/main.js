739bf6fd12cf34fd7388e30081c2a69399d6bcaf
Sandra Ustach <98275838+ustach@users.noreply.github.com>
2022-04-03 00:37:21 +0100

    Add files via upload

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+window.alert("Hello!"); 
+
+function myFunction() {
+    var sentence = "My name is: "; // Defining a variable and giving it a string value
+sentence += " Sandra Ustach"; // Using += operator to concatenate a string
+document.getElementById("Concatenate").innerHTML = sentence; //Putting the value 
+} //of resulr into HTML element with "Concatenate" id
