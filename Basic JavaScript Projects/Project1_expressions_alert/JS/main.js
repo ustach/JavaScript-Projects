72f0246b805d41142ea1de8c5d385b8f7d71a36b
Sandra Ustach <98275838+ustach@users.noreply.github.com>
2022-04-02 23:57:29 +0100

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+window.alert("Hello!"); 
+
+var A = " Sandra "; // Defining first variable and giving it a string value
+var B = " Ustach "; // Defining second variable and giving it a string value
+var result = A + B ; //Defining result of 1st and 2nd variable
+document.write("<br>My name is: "+ result);
+
+var A = "I am blue."
+var A = A.fontcolor("blue"); //Using the fontcolor method on A variable
+document.write(A);
